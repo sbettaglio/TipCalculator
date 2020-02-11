82ba553b6513e820fdc4c68b1acb45a0194f5348
Stefan Bettaglio <sbettaglio@gmail.com>
2020-02-11 11:37:59 -0500

    fixed string error and letter case error

--- Program.cs
+++ Program.cs
@@ -11,10 +11,20 @@ namespace TipCalculator
       //Ask how much the bill
       Console.WriteLine("How much was your meal?");
       var total = Console.ReadLine();
+      //Check if the input was correct (a number)
+      double billTotal;
+      var isValid = Double.TryParse(total, out billTotal);
+      if (!isValid)
+      {
+        Console.WriteLine("That is not a number, try again");
+        total = Console.ReadLine();
+        billTotal = Double.Parse(total);
+      }
       //How was the service?
       Console.WriteLine("How was your service?");
       Console.WriteLine("Was it okay, good or great?");
-      var service = Console.ReadLine();
+      var service = Console.ReadLine().ToLower();
+      //service = service.ToLower();  *this works as well as the above*dot
       //okay, good, great, default
 
       //calculate the tip based on the level service
@@ -32,10 +42,11 @@ namespace TipCalculator
         tipPercentage = 0.25;
       }
 
-      //calculate 18%
-      var tip = double.Parse(total) * tipPercentage;
+      //calculate tip%
+
+      var tip = billTotal * tipPercentage;
       // add the tip to the total
-      var grandTotal = tip + double.Parse(total);
+      var grandTotal = tip + billTotal;
       //display the result
       //tipPercent * 100 will give you the correct value
       var tipToDisplay = tipPercentage * 100;

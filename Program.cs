8f1bd1c7b1dfa508eeff6648bc6f02187e232e1b
Stefan Bettaglio <sbettaglio@gmail.com>
2020-02-11 11:01:23 -0500

    Added service lever for tip

--- Program.cs
+++ Program.cs
@@ -11,13 +11,36 @@ namespace TipCalculator
       //Ask how much the bill
       Console.WriteLine("How much was your meal?");
       var total = Console.ReadLine();
+      //How was the service?
+      Console.WriteLine("How was your service?");
+      Console.WriteLine("Was it okay, good or great?");
+      var service = Console.ReadLine();
+      //okay, good, great, default
+
+      //calculate the tip based on the level service
+      var tipPercentage = 0.15;
+      if (service == "okay")
+      {
+        tipPercentage = 0.18;
+      }
+      else if (service == "good")
+      {
+        tipPercentage = 0.20;
+      }
+      else if (service == "great")
+      {
+        tipPercentage = 0.25;
+      }
+
       //calculate 18%
-      var tip = double.Parse(total) * 0.18;
+      var tip = double.Parse(total) * tipPercentage;
       // add the tip to the total
       var grandTotal = tip + double.Parse(total);
       //display the result
+      //tipPercent * 100 will give you the correct value
+      var tipToDisplay = tipPercentage * 100;
       Console.WriteLine("Your total was " + total);
-      Console.WriteLine("Your 18% tip is " + tip);
+      Console.WriteLine("Your " + tipToDisplay + "% tip is " + tip);
       Console.WriteLine("Grand total is " + grandTotal);
 
     }
